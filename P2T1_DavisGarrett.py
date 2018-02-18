ebd0d3756287014297b4e9b9d2e055b27a9910c0
garrettDavis718 <36609502+garrettDavis718@users.noreply.github.com>
2018-02-18 17:27:28 -0500

    Add files via upload

---
+++ P2T1_DavisGarrett.py
@@ -0,0 +1,11 @@
+# CTI-110 
+# P2T1 - Sales Prediction 
+# Garrett Davis   
+# 02/12/18
+
+num1=int(input("Please enter your projected annual sales: "))
+
+
+num2 = int(num1 * .23)
+
+print("Your annual profit will be: ", num2)
